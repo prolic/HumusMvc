c88ba93b4f04331a918e9e69606b22d4d5455a29
prolic <saschaprolic@googlemail.com>
2012-11-16 20:46:26 +0100

    added escape(LC_MESSAGES) method to HumusMvc\View\View

--- src/HumusMvc/View/View.php
+++ src/HumusMvc/View/View.php
@@ -264,4 +264,18 @@ class View implements ViewInterface
         return $this->getHelperPluginManager()->get($name);
     }
 
+    /**
+     * Escapes a value for output in a view script.
+     *
+     * If escaping mechanism is one of htmlspecialchars or htmlentities, uses
+     * {@link $_encoding} setting.
+     *
+     * @param mixed $var The output to escape.
+     * @return mixed The escaped value.
+     */
+    public function escape($var)
+    {
+        return $this->_view->escape($var);
+    }
+
 }
